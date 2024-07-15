0c4f7ab7b8c2b53b5912db6bd5de39aea7bf641f
Nikolozabesadze <abesadzenikolozi206@gmail.com>
2024-07-15 20:04:20 +0400

    commit

---
+++ level17/classwork/classwork.py
@@ -0,0 +1,19 @@
+x = int(input('1 : 1000 :'))
+print('correct')
+
+food1 = input('first ingredient how many?')
+food2 = input('ingredient2 how many?')
+food3 = input("ingredient3 how many?")
+food4 = input('ingredient4 how many?')
+
+food11 = input('how much it costs?: ')
+food12 = input('how much it costs?: ')
+food13 = input("how much it costs?: ")
+food14 = input('how much it costs?: ')
+
+food11 = 5
+food12 = 10
+food13 = 20
+food14 = 40
+
+print(food11 + food12 + food13 + food14 )
