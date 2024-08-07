488002d1f2db9c67a1223ef7cc6bd66d96a10a65
Nikolozabesadze <abesadzenikolozi206@gmail.com>
2024-08-07 19:43:25 +0400

    commit

--- level17/homework/homework.py
+++ level17/homework/homework.py
@@ -6,4 +6,4 @@ print(num1 + num2)
 first_semester = int(input('write the grade which you got at first semester'))
 second_semester = int(input('write the grade which you got at second semester'))
 
-print( (first_semester + second_semester)/2 )
+print( (first_semester + second_semester) / 2)
---
+++ level21/classwork/classwork.py
@@ -0,0 +1,5 @@
+#message to be displayed
+message = 'fasten y'
+# for loop to dysplay the message 3 times
+for i in range(  3  ):
+    print(message)
